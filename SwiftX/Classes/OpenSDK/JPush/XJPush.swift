5b49ca76793a435824f47f2215f42609bba27d84
wangcong <wangcong@medlinker.com>
2019-03-18 02:12:58 +0800

    修改XJPush

--- SwiftX/Classes/OpenSDK/JPush/XJPush.swift
+++ SwiftX/Classes/OpenSDK/JPush/XJPush.swift
@@ -19,44 +19,44 @@ final public class XJPush: NSObject {
     
     // 在调用前必须注册
     public func register(appKey: String, launchOptions: [AnyHashable: Any], complection:((_ registrationID: String?) -> Void)? = nil) {
-        let entity = JPUSHRegisterEntity()
-        if #available(iOS 12.0, *) {
-            entity.types = Int(JPAuthorizationOptions(rawValue: JPAuthorizationOptions.alert.rawValue | JPAuthorizationOptions.badge.rawValue | JPAuthorizationOptions.sound.rawValue | JPAuthorizationOptions.providesAppNotificationSettings.rawValue).rawValue)
-        } else {
-            entity.types = Int(JPAuthorizationOptions(rawValue: JPAuthorizationOptions.alert.rawValue | JPAuthorizationOptions.badge.rawValue | JPAuthorizationOptions.sound.rawValue).rawValue)
-        }
-        JPUSHService.register(forRemoteNotificationConfig: entity, delegate: nil)
-
-        var isProduction = false
-        #if !DEBUG
-        isProduction = true
-        #endif
-        JPUSHService.setup(withOption: launchOptions, appKey: appKey, channel: "Publish channel", apsForProduction: isProduction)
-        JPUSHService.registrationIDCompletionHandler { [weak self] (code, registrationID) in
-            if code == 0 {
-                self?.registrationID = registrationID
-                complection?(registrationID)
-                #if DEBUG
-                NSLog("com.SwiftX.OpenSDK.XJPush    registrationIDCompletionHandler   success  \(registrationID)")
-                #endif
-            } else {
-                #if DEBUG
-                NSLog("com.SwiftX.OpenSDK.XJPush    registrationIDCompletionHandler    fialure   \(code)")
-                #endif
-            }
-        }
+//        let entity = JPUSHRegisterEntity()
+//        if #available(iOS 12.0, *) {
+//            entity.types = Int(JPAuthorizationOptions(rawValue: JPAuthorizationOptions.alert.rawValue | JPAuthorizationOptions.badge.rawValue | JPAuthorizationOptions.sound.rawValue | JPAuthorizationOptions.providesAppNotificationSettings.rawValue).rawValue)
+//        } else {
+//            entity.types = Int(JPAuthorizationOptions(rawValue: JPAuthorizationOptions.alert.rawValue | JPAuthorizationOptions.badge.rawValue | JPAuthorizationOptions.sound.rawValue).rawValue)
+//        }
+//        JPUSHService.register(forRemoteNotificationConfig: entity, delegate: nil)
+//
+//        var isProduction = false
+//        #if !DEBUG
+//        isProduction = true
+//        #endif
+//        JPUSHService.setup(withOption: launchOptions, appKey: appKey, channel: "Publish channel", apsForProduction: isProduction)
+//        JPUSHService.registrationIDCompletionHandler { [weak self] (code, registrationID) in
+//            if code == 0 {
+//                self?.registrationID = registrationID
+//                complection?(registrationID)
+//                #if DEBUG
+//                NSLog("com.SwiftX.OpenSDK.XJPush    registrationIDCompletionHandler   success  \(registrationID)")
+//                #endif
+//            } else {
+//                #if DEBUG
+//                NSLog("com.SwiftX.OpenSDK.XJPush    registrationIDCompletionHandler    fialure   \(code)")
+//                #endif
+//            }
+//        }
     }
     
     public func register(deviceToken: Data) {
-        JPUSHService.registerDeviceToken(deviceToken)
+//        JPUSHService.registerDeviceToken(deviceToken)
     }
     
     public func handleRemoteNotification(_ userInfo: [AnyHashable: Any]) {
-        JPUSHService.handleRemoteNotification(userInfo)
+//        JPUSHService.handleRemoteNotification(userInfo)
     }
     
     public func showLocalNotificationAtFront(_ notification: UILocalNotification) {
-        JPUSHService.showLocalNotification(atFront: notification, identifierKey: nil)
+//        JPUSHService.showLocalNotification(atFront: notification, identifierKey: nil)
     }
     
     public func handleOpen(url: URL) -> Bool {
@@ -69,33 +69,33 @@ final public class XJPush: NSObject {
 extension XJPush {
     
     public func addTags(_ tags: Set<String>, _ complection: ((_ iResCode: Int, _ iTags: Set<AnyHashable>?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.addTags(tags, completion: { (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
-            complection?(iResCode, iTags, seq)
-        }, seq: seq)
+//        JPUSHService.addTags(tags, completion: { (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
+//            complection?(iResCode, iTags, seq)
+//        }, seq: seq)
     }
     
     public func setTags(_ tags: Set<String>, _ complection: ((_ iResCode: Int, _ iTags: Set<AnyHashable>?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.setTags(tags, completion: { (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
-            complection?(iResCode, iTags, seq)
-        }, seq: seq)
+//        JPUSHService.setTags(tags, completion: { (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
+//            complection?(iResCode, iTags, seq)
+//        }, seq: seq)
     }
     
     public func deleteTags(_ tags: Set<String>, _ complection: ((_ iResCode: Int, _ iTags: Set<AnyHashable>?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.deleteTags(tags, completion: { (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
-            complection?(iResCode, iTags, seq)
-        }, seq: seq)
+//        JPUSHService.deleteTags(tags, completion: { (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
+//            complection?(iResCode, iTags, seq)
+//        }, seq: seq)
     }
     
     public func cleanTags(_ complection: ((_ iResCode: Int, _ iTags: Set<AnyHashable>?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.cleanTags({ (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
-            complection?(iResCode, iTags, seq)
-        }, seq: seq)
+//        JPUSHService.cleanTags({ (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
+//            complection?(iResCode, iTags, seq)
+//        }, seq: seq)
     }
     
     public func getAllTags(_ complection: ((_ iResCode: Int, _ iTags: Set<AnyHashable>?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.getAllTags({ (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
-            complection?(iResCode, iTags, seq)
-        }, seq: seq)
+//        JPUSHService.getAllTags({ (iResCode: Int, iTags: Set<AnyHashable>?, seq: Int) in
+//            complection?(iResCode, iTags, seq)
+//        }, seq: seq)
     }
     
 }
@@ -104,61 +104,60 @@ extension XJPush {
 extension XJPush {
     
     public func setAlias(_ alias: String, _ complection: ((_ iResCode: Int, _ iAlias: String?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.setAlias(alias, completion: { (iResCode, iAlias, seq) in
-            complection?(iResCode, iAlias, seq)
-        }, seq: seq)
+//        JPUSHService.setAlias(alias, completion: { (iResCode, iAlias, seq) in
+//            complection?(iResCode, iAlias, seq)
+//        }, seq: seq)
     }
     
     public func deleteAlias(_ complection: ((_ iResCode: Int, _ iAlias: String?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.deleteAlias({ (iResCode, iAlias, seq) in
-            complection?(iResCode, iAlias, seq)
-        }, seq: seq)
+//        JPUSHService.deleteAlias({ (iResCode, iAlias, seq) in
+//            complection?(iResCode, iAlias, seq)
+//        }, seq: seq)
     }
     
     public func getAlias(_ complection: ((_ iResCode: Int, _ iAlias: String?, _ seq: Int) -> Void)?, _ seq: Int = 0) {
-        JPUSHService.getAlias({ (iResCode, iAlias, seq) in
-            complection?(iResCode, iAlias, seq)
-        }, seq: seq)
+//        JPUSHService.getAlias({ (iResCode, iAlias, seq) in
+//            complection?(iResCode, iAlias, seq)
+//        }, seq: seq)
     }
     
 }
 
-extension XJPush: JPUSHRegisterDelegate {
-
-    @available(iOS 10.0, *)
-    public func jpushNotificationCenter(_ center: UNUserNotificationCenter!, willPresent notification: UNNotification!, withCompletionHandler completionHandler: ((Int) -> Void)!) {
-        let userInfo = notification.request.content.userInfo
-        if notification.request.trigger is UNPushNotificationTrigger {
-            JPUSHService.handleRemoteNotification(userInfo)
-        } else {
-            //本地通知
-        }
-        //需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以选择设置
-        completionHandler(Int(UNNotificationPresentationOptions.alert.rawValue))
-    }
-    
-    @available(iOS 10.0, *)
-    public func jpushNotificationCenter(_ center: UNUserNotificationCenter!, didReceive response: UNNotificationResponse!, withCompletionHandler completionHandler: (() -> Void)!) {
-        let userInfo = response.notification.request.content.userInfo
-        if response.notification.request.trigger is UNPushNotificationTrigger {
-            JPUSHService.handleRemoteNotification(userInfo)
-        } else {
-            //本地通知
-        }
-        //处理通知 跳到指定界面等等
-//        receivePush(userInfo as! Dictionary<String, Any>)
-        completionHandler()
-    }
-    
-    @available(iOS 12.0, *)
-    public func jpushNotificationCenter(_ center: UNUserNotificationCenter!, openSettingsFor notification: UNNotification?) {
-        
-    }
-    
-    // iOS 9.0
-//    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-//        JPUSHService.handleRemoteNotification(userInfo);
-//        completionHandler(UIBackgroundFetchResult.newData);
+//extension XJPush: JPUSHRegisterDelegate {
+//
+//    @available(iOS 10.0, *)
+//    public func jpushNotificationCenter(_ center: UNUserNotificationCenter!, willPresent notification: UNNotification!, withCompletionHandler completionHandler: ((Int) -> Void)!) {
+//        let userInfo = notification.request.content.userInfo
+//        if notification.request.trigger is UNPushNotificationTrigger {
+//            JPUSHService.handleRemoteNotification(userInfo)
+//        } else {
+//            //本地通知
+//        }
+//        //需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以选择设置
+//        completionHandler(Int(UNNotificationPresentationOptions.alert.rawValue))
 //    }
-    
-}
+//
+//    @available(iOS 10.0, *)
+//    public func jpushNotificationCenter(_ center: UNUserNotificationCenter!, didReceive response: UNNotificationResponse!, withCompletionHandler completionHandler: (() -> Void)!) {
+//        let userInfo = response.notification.request.content.userInfo
+//        if response.notification.request.trigger is UNPushNotificationTrigger {
+//            JPUSHService.handleRemoteNotification(userInfo)
+//        } else {
+//            //本地通知
+//        }
+//        //处理通知 跳到指定界面等等
+//        completionHandler()
+//    }
+//
+//    @available(iOS 12.0, *)
+//    public func jpushNotificationCenter(_ center: UNUserNotificationCenter!, openSettingsFor notification: UNNotification?) {
+//
+//    }
+//
+//    // iOS 9.0
+////    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+////        JPUSHService.handleRemoteNotification(userInfo);
+////        completionHandler(UIBackgroundFetchResult.newData);
+////    }
+//
+//}
---
+++ SwiftX/Classes/View/Custom/XBadgeView.swift
@@ -0,0 +1,12 @@
+//
+//  XBadgeView.swift
+//  SwiftX
+//
+//  Created by wangcong on 2019/3/18.
+//
+
+import UIKit
+
+class XBadgeView: NSObject {
+
+}
