9ec0772cce305a324c60caeb852225abfe3a4481
wangcong <wangcong@medlinker.com>
2019-06-02 11:11:20 +0800

    验证码输入框支持password

--- SwiftX/Classes/View/Custom/XValidationTextField.swift
+++ SwiftX/Classes/View/Custom/XValidationTextField.swift
@@ -23,6 +23,11 @@ import UIKit
 }
 
 public class XValidationTextField: UIView, UIKeyInput, UITextInputTraits {
+
+    public enum ValidationType {
+        case text
+        case password
+    }
     
     private var _length: Int = 6
     public var length: Int {
@@ -34,6 +39,26 @@ public class XValidationTextField: UIView, UIKeyInput, UITextInputTraits {
             self.setNeedsDisplay()
         }
     }
+    public var lineColor: UIColor = UIColor.gray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    public var lineHighlightedColor: UIColor = UIColor(hexColor: "66B30C") {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    public var validationType: ValidationType = .text {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    weak open var delegate: XValidationTextFieldDelegate?
+
+    /// MARK: 仅validationType == .text 有效
     private var _text: String = ""
     public var text: String {
         get {
@@ -54,24 +79,12 @@ public class XValidationTextField: UIView, UIKeyInput, UITextInputTraits {
             setNeedsDisplay()
         }
     }
-    
-    public var lineColor: UIColor = UIColor.gray {
-        didSet {
-            setNeedsDisplay()
-        }
-    }
-    public var lineHighlightedColor: UIColor = UIColor(hexColor: "66B30C") {
-        didSet {
-            setNeedsDisplay()
-        }
-    }
     public var itemSpacing: CGFloat = 15 {
         didSet {
             setNeedsDisplay()
         }
     }
-    weak open var delegate: XValidationTextFieldDelegate?
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         _init()
@@ -107,37 +120,85 @@ public class XValidationTextField: UIView, UIKeyInput, UITextInputTraits {
         context?.setLineCap(.round)
         context?.setStrokeColor(lineColor.cgColor)
         context?.setFillColor(lineColor.cgColor)
-        
-        // 绘制横线
-        for index in 0 ..< _length {
-            let highlightedIndex = _text.count != _length ? _text.count : _text.count - 1
-            if index == highlightedIndex {
-                context?.saveGState()
-                context?.setLineWidth(2)
-                context?.setStrokeColor(lineHighlightedColor.cgColor)
-                context?.setFillColor(lineHighlightedColor.cgColor)
+
+        switch validationType {
+        case .password:
+            // 绘制横线
+            for index in 0 ..< _length {
                 context?.move(to: CGPoint(x: CGFloat(index) * eachWidth, y: eachHeight))
-                context?.addLine(to: CGPoint(x: CGFloat(index) * eachWidth + (eachWidth - itemSpacing), y: eachHeight))
-                context?.drawPath(using: .fillStroke)
-                context?.restoreGState()
-            } else {
-                context?.move(to: CGPoint(x: CGFloat(index) * eachWidth, y: eachHeight))
-                context?.addLine(to: CGPoint(x: CGFloat(index) * eachWidth + (eachWidth - itemSpacing), y: eachHeight))
+                let highlightedIndex = _text.count != _length ? _text.count : _text.count - 1
+                if index == highlightedIndex {
+                    context?.saveGState()
+                    context?.setLineWidth(2)
+                    context?.setStrokeColor(lineHighlightedColor.cgColor)
+                    context?.setFillColor(lineHighlightedColor.cgColor)
+                    context?.addLine(to: CGPoint(x: CGFloat(index) * eachWidth + (eachWidth - itemSpacing), y: eachHeight))
+                    context?.drawPath(using: .fillStroke)
+                    context?.restoreGState()
+                } else {
+                    context?.addLine(to: CGPoint(x: CGFloat(index) * eachWidth + (eachWidth - itemSpacing), y: eachHeight))
+                    context?.drawPath(using: .fillStroke)
+                }
+            }
+
+            // 绘制文字
+            for (index, c) in _text.enumerated() {
+                let style = NSMutableParagraphStyle()
+                style.alignment = .center
+                let attributes = [
+                    NSAttributedString.Key.foregroundColor: textColor,
+                    NSAttributedString.Key.font: font,
+                    NSAttributedString.Key.paragraphStyle: style
+                ]
+                (String(c) as NSString).draw(in: CGRect(x: CGFloat(index) * eachWidth, y: 0, width: eachWidth - itemSpacing, height: eachHeight), withAttributes: attributes)
+            }
+        default:
+            // 绘制边框
+            for index in 0 ..< _length {
+                context?.move(to: CGPoint(x: CGFloat(index) * eachWidth, y: 0))
+                let rect = CGRect(x: CGFloat(index) * eachWidth, y: 0, width: eachWidth, height: eachHeight)
+                let highlightedIndex = _text.count != _length ? _text.count : _text.count - 1
+                if index == highlightedIndex {
+                    context?.saveGState()
+                    context?.setLineWidth(2)
+                    context?.setStrokeColor(lineHighlightedColor.cgColor)
+                    context?.setFillColor(lineHighlightedColor.cgColor)
+
+                    switch index {
+                    case 0:
+                        context?.addPath(UIBezierPath(roundedRect: rect, byRoundingCorners: UIRectCorner(rawValue: UIRectCorner.topLeft.rawValue | UIRectCorner.bottomLeft.rawValue), cornerRadii: CGSize(width: 5, height: 5)).cgPath)
+                    case _length - 1:
+                        context?.addPath(UIBezierPath(roundedRect: rect, byRoundingCorners: UIRectCorner(rawValue: UIRectCorner.topRight.rawValue | UIRectCorner.bottomRight.rawValue), cornerRadii: CGSize(width: 5, height: 5)).cgPath)
+                    default:
+                        context?.addRect(CGRect(x: CGFloat(index) * eachWidth, y: 0, width: eachWidth, height: eachHeight))
+                    }
+
+                    context?.drawPath(using: .fillStroke)
+                    context?.restoreGState()
+                } else {
+                    switch index {
+                    case 0:
+                        context?.addPath(UIBezierPath(roundedRect: rect, byRoundingCorners: UIRectCorner(rawValue: UIRectCorner.topLeft.rawValue | UIRectCorner.bottomLeft.rawValue), cornerRadii: CGSize(width: 5, height: 5)).cgPath)
+                    case _length - 1:
+                        context?.addPath(UIBezierPath(roundedRect: rect, byRoundingCorners: UIRectCorner(rawValue: UIRectCorner.topRight.rawValue | UIRectCorner.bottomRight.rawValue), cornerRadii: CGSize(width: 5, height: 5)).cgPath)
+                    default:
+                        context?.addRect(CGRect(x: CGFloat(index) * eachWidth, y: 0, width: eachWidth, height: eachHeight))
+                    }
+                    context?.drawPath(using: .fillStroke)
+                }
+            }
+
+            // 绘制圆点
+            context?.saveGState()
+            context?.setLineWidth(8)
+            context?.setStrokeColor(UIColor.darkText.cgColor)
+            context?.setFillColor(UIColor.darkText.cgColor)
+            for (index, _) in _text.enumerated() {
+                context?.addArc(center: CGPoint(x: CGFloat(index) * eachWidth + eachWidth / 2.0, y: eachHeight / 2.0), radius: 8, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
                 context?.drawPath(using: .fillStroke)
             }
         }
 
-        // 绘制文字
-        for (index, c) in _text.enumerated() {
-            let style = NSMutableParagraphStyle()
-            style.alignment = .center
-            let attributes = [
-                NSAttributedString.Key.foregroundColor: textColor,
-                NSAttributedString.Key.font: font,
-                NSAttributedString.Key.paragraphStyle: style
-            ]
-            (String(c) as NSString).draw(in: CGRect(x: CGFloat(index) * eachWidth, y: 0, width: eachWidth - itemSpacing, height: eachHeight), withAttributes: attributes)
-        }
 
     }
     
